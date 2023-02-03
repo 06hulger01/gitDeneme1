b87626f323f65af96239d8e79194b817a1eda542
06hulger01 <haciulger1980@gmail.com>
2023-02-03 16:53:25 +0300

    added tree message

--- src/main/java/gitDeneme1.java
+++ src/main/java/gitDeneme1.java
@@ -2,5 +2,6 @@ public class gitDeneme1 {
     public static void main(String[] args) {
         System.out.println("hello java");
         System.out.println("hello friends");
+        System.out.println("hello dear");
     }
 }
